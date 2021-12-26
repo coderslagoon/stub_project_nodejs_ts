32e45bd4ade619de613a057ccf0caea5b3f7bd74
Markus Hahn <mhahn@mailbox.org>
2021-12-26 01:34:48 +0100

    try how to xo

--- src/stub_project_nodejs_ts/main.ts
+++ src/stub_project_nodejs_ts/main.ts
@@ -1,27 +1,33 @@
+import {createRequire} from "node:module";
 
 import fse from "fs-extra";
 import _ from "lodash";
 
-import {createRequire} from "module";
 const require = createRequire(import.meta.url);
 
+/* eslint-disable-next-line prettier/prettier, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires */
 const data: Record<string, unknown> = require("../../src/stub_project_nodejs_ts/data.json");
 
 async function main(): Promise<void> {
     try {
         await fse.access(".");
         console.log("GOOD!");
-    } catch (err) {
-        console.error("this is unexpected !?!", (err as Error)?.message ?? err);
+    } catch (error: unknown) {
+        console.error(
+            "this is unexpected !?!",
+            (error as Error)?.message ?? error,
+        );
     }
+
     const items = await fse.readdir(".");
-    _.map(items, item => {
+    _.map(items, (item) => {
         console.info(item);
     });
     console.log(JSON.stringify(data, null, 4));
 }
 
-main().catch(err => {
-    console.error("UNCAUGHT ERROR:", err?.message ?? err);
+main().catch((error) => {
+    console.error("UNCAUGHT ERROR:", error?.message ?? error);
+    /* eslint-disable-next-line node/prefer-global/process, unicorn/no-process-exit */
     process.exit(1);
 });
